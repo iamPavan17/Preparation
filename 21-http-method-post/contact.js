e2bd5c16d9dca9c42d5e4a297c91f95b3c88c8b9
iamPavan17 <iampavan05@gmail.com>
2019-08-07 23:00:16 +0530

    added validations

--- 21-http-method-post/contact.js
+++ 21-http-method-post/contact.js
@@ -11,15 +11,38 @@ function ContactForm(data) {
             mobile: this.phone,
             message: this.message
         };
-        var axios = require('axios');
-        var url = 'http://dct-api-data.herokuapp.com/contact_forms.json'
-        axios.post(url, dataToSend)
-         .then(function(response) {
-            console.log(response.data);
-        })
-        .catch(err => {
-            console.log(err.response.data);
-        });
+
+        var errObj = {};
+        let flag = false;
+        let nameVal = /^[a-zA-Z ]+$/.test(dataToSend.name);
+        if(!nameVal) {
+            errObj['name'] = 'Name should contain only characters';
+            flag = false;
+        } else {
+            flag = true;
+        }
+
+        let phoneVal = /^\d{10}$/.test(dataToSend.mobile);
+        if(!phoneVal) {
+            errObj['phone'] = 'Phone number should be only 10 digits.'
+            flag = false;
+        } else {
+            flag = true;
+        }
+
+        if(flag) {
+            var axios = require('axios');
+            var url = 'http://dct-api-data.herokuapp.com/contact_forms.json'
+            axios.post(url, dataToSend)
+            .then(function(response) {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log(err.response.data);
+            });
+        } else {
+            console.log(errObj);
+        }
     }
 }
 
