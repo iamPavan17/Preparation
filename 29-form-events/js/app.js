befc847d5d8d9f1b72e2e1f65ad1ccc953528872
iamPavan17 <iampavan05@gmail.com>
2019-08-12 19:51:12 +0530

    29-form-client-validation

---
+++ 29-form-events/js/app.js
@@ -0,0 +1,35 @@
+var nameHandler = document.getElementById('name');
+var mobileHandler = document.getElementById('mobile');
+var emailHandler = document.getElementById('email');
+var expHanadler = document.getElementById('exp');
+var aboutHandler = document.getElementById('about');
+
+  var course = {
+      js: ['MERN', 'NodeJS', 'React'],
+      py: ['Django'],
+      rb: ['Rails']
+  };
+
+  var langHandler = document.getElementById('lang');
+  var crsHandler = document.getElementById('crs');
+
+  langHandler.addEventListener('change', function() {
+      // console.log(langHandler.value);
+      var courseSelected = langHandler.value;
+      crsHandler.innerHTML = "";
+      course[courseSelected].forEach(c => {
+        var option = document.createElement('option');
+        var optionText = document.createTextNode(c);
+        option.setAttribute('value', c);
+        option.appendChild(optionText);
+        crsHandler.appendChild(option);
+      });
+  });
+
+  nameHandler.addEventListener('blur', function() {
+    var url = `https://gender-api.com/get?name=${nameHandler.value}&key=SBrwJgoFzvJcoBbmMr`;
+    axios.get(url).then(function(response) {
+        // console.log(response.data.gender)
+        document.getElementById(response.data.gender).setAttribute('checked', true);
+    });
+  });
